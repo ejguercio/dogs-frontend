c323c3a93334ea492c551bb081eed6ef5a16f653
Edgar Guercio <eguercio.training@innevo.cl>
2024-04-24 00:23:37 -0300

    ADD environment urlbase api

--- src/app/dogs/dog.service.ts
+++ src/app/dogs/dog.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Dog } from '../models/dog.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DogService {
-  private apiUrl = 'https://dogs-production-82a6.up.railway.app/dogs';
-
+  private urlBase: string = `${environment.urlBase}dogs`;
   constructor(private http: HttpClient) {}
 
   getDogs(): Observable<Dog[]> {
-    return this.http.get<Dog[]>(this.apiUrl);
+    return this.http.get<Dog[]>(this.urlBase);
   }
 
   getDogById(id: number | string): Observable<Dog[]> {
-    return this.http.get<Dog[]>(`${this.apiUrl}/${id}`);
+    return this.http.get<Dog[]>(`${this.urlBase}/${id}`);
   }
 }
